f0817429956c1851cbc57229197c10ef5e0ebb9a
Liliumgwa <joao.vitor.guedes21@gmail.com>
2026-06-16 19:16:06 -0300

    feat: add fields and getter/setter methods to Professor class

--- Professor.java
+++ Professor.java
@@ -1,3 +1,31 @@
 public class Professor {
 
+    private String nome;
+    private String competencia;
+    private boolean disponibilidade;
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCompetencia() {
+        return competencia;
+    }
+
+    public void setCompetencia(String competencia) {
+        this.competencia = competencia;
+    }
+
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
 }
